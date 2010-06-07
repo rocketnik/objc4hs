5b69fb0790912a0932379b1bc9db8f7271e65f3e
Nikolas Mayr <n.mayr@icp-beratung.de>
2010-06-07 16:06:17 +0200

    renamed functions. added a bunch of functions. sill some missing.

--- src/OHVector.h
+++ src/OHVector.h
@@ -1,13 +1,53 @@
-CGPoint  addP(CGPoint a, CGPoint b);
-CGPoint  subP(CGPoint a, CGPoint b);
-CGPoint diffP(CGPoint a, CGPoint b);
-CGPoint  negP(CGPoint a);
-CGPoint sMulP(float   s, CGPoint a);
-CGPoint compMul(CGPoint a, CGPoint b);
-CGPoint compInverseP(CGPoint a);
+#import <Foundation/Foundation.h>
 
-float   distP(CGPoint a, CGPoint b); // euklidian distance
-int quadrantP(CGPoint a);            // quadrant
-int   octantP(CGPoint a);            // oktant
-CGSize  sMulS(float   s, CGSize  a);
-CGRect    mvR(CGRect  r, CGPoint p);
+typedef struct CGPoint *pCGPoint;
+typedef struct CGSize  *pCGSize;
+typedef struct CGRect  *pCGRect;
+
+CGPoint ptAdd(    CGPoint  a, CGPoint  b);
+CGPoint ptSub(    CGPoint  a, CGPoint  b);
+CGPoint ptDiff(   CGPoint  a, CGPoint  b);
+CGPoint ptNeg(    CGPoint  a);
+void    ptNegInpl(pCGPoint a);
+
+CGPoint ptScale(    float  s, CGPoint  a);
+void    ptScaleInpl(float  s, pCGPoint a);
+CGPoint ptDiv(    CGPoint  a,   float  s);
+void    ptDivInpl(pCGPoint a,   float  s);
+CGPoint ptCompMul(CGPoint  a, CGPoint  b);
+CGPoint ptCompDiv(CGPoint  a, CGPoint  b);
+CGPoint ptInv(    CGPoint  a);
+void    ptInvInpl(pCGPoint a);
+
+double  ptNorm(CGPoint a);
+CGPoint ptNormalize(     CGPoint a);
+void    ptNormalizeInpl(pCGPoint a);
+
+
+CGSize szAdd(     CGSize  a, CGSize  b);
+CGSize szSub(     CGSize  a, CGSize  b);
+CGSize szDiff(    CGSize  a, CGSize  b);
+CGSize szNeg(     CGSize  a);
+void   szNegInpl( pCGSize a);
+
+CGSize szScale(    float  s, CGSize  a);
+void   szScaleInpl(float  s, pCGSize a);
+CGSize szDiv(     CGSize  a,  float  s);
+void   szDivInpl( pCGSize a,  float  s);
+CGSize szCompMul( CGSize  a, CGSize  b);
+CGSize szCompDiv( CGSize  a, CGSize  b);
+CGSize szInv(     CGSize  a);
+void   szInvInpl( pCGSize a);
+
+float  szNorm(CGSize a);            // euklidian norm
+CGSize szNomalize(     CGSize  a);
+void   szNormalizeInpl(pCGSize a);
+
+
+float ptDist(CGPoint a, CGPoint b); // euklidian distance
+int   ptQuadrant(CGPoint a); // quadrant
+int   ptOctant(  CGPoint a); // oktant
+
+CGRect rectMv(    CGRect  r, CGPoint p);
+void   rectMvInpl(pCGRect r, CGPoint p);
+
--- src/OHVector.m
+++ src/OHVector.m
@@ -1,36 +1,48 @@
 #import "OHVector.h"
 
-CGPoint addP(CGPoint a, CGPoint b) {
-	return CGPointMake(a.x+b.x, a.y+b.y);
-}
+CGPoint ptAdd(    CGPoint  a, CGPoint  b)  { return CGPointMake(    a.x+b.x,     a.y+b.y); }
+CGPoint ptSub(    CGPoint  a, CGPoint  b)  { return CGPointMake(    a.x-b.x,     a.y-b.y); }
+CGPoint ptDiff(   CGPoint  a, CGPoint  b)  { return CGPointMake(   -a.x+b.x,    -a.y+b.y); }
+CGPoint ptNeg(    CGPoint  a)              { return CGPointMake(   -a.x    ,    -a.y    ); }
+void    ptNegInpl(pCGPoint a)              { a->x = -a->x; a->y = -a->y; }
 
-CGPoint subP(CGPoint a, CGPoint b) {
-	return addP(a, negP(b));
-}
+CGPoint ptScale(    float  s, CGPoint  a)  { return CGPointMake(  s*a.x    ,   s*a.y    ); }
+void    ptScaleInpl(float  s, pCGPoint a)  { a->x *= s; a->y *= s; }
+CGPoint ptDiv(    CGPoint  a,   float  s)  { return CGPointMake(    a.x/s  ,     a.y/s  ); }
+void    ptDivInpl(pCGPoint a,   float  s)  { a->x /= s; a->y /= s; }
+CGPoint ptCompMul(CGPoint  a, CGPoint  b)  { return CGPointMake(    a.x*b.x,     a.y*b.y); }
+CGPoint ptCompDiv(CGPoint  a, CGPoint  b)  { return CGPointMake(    a.x/b.x,     a.y/b.y); }
+CGPoint ptInv(    CGPoint  a)              { return CGPointMake(1.f/a.x    , 1.f/a.y    ); }
+void    ptInvInpl(pCGPoint a)              { a->x = 1.f/a->x; a->y = 1.f/a->y; }
 
-CGPoint diffP(CGPoint a, CGPoint b) {
-	return subP(b,a);
-}
+double  ptNorm(CGPoint a)                  { return sqrt(a.x*a.x + a.y*a.y); }
+CGPoint ptNormalize(    CGPoint  a)        { float n = sqrt(a.x*a.x + a.y*a.y); return CGPointMake(a.x/n, a.y/n); }
+void    ptNormalizeInpl(pCGPoint a)        { float n = sqrt(a->x*a->x + a->y*a->y); a->x /= n; a->y /= n; }
 
-CGPoint negP(CGPoint a) {
-	return sMulP(-1.f,a);
-}
 
-CGPoint sMulP(float s, CGPoint a) {
-	return CGPointMake(s*a.x, s*a.y);
-}
+CGSize szAdd(     CGSize  a, CGSize  b)    { return CGSizeMake(    a.width+b.width,     a.height+b.height); }
+CGSize szSub(     CGSize  a, CGSize  b)    { return CGSizeMake(    a.width-b.width,     a.height-b.height); }
+CGSize szDiff(    CGSize  a, CGSize  b)    { return CGSizeMake(   -a.width+b.width,    -a.height+b.height); }
+CGSize szNeg(     CGSize  a)               { return CGSizeMake(   -a.width        ,    -a.height         ); }
+void   szNegInpl( pCGSize a)               { a->width = -a->width; a->height = -a->height; }
 
-CGPoint compMul(CGPoint a, CGPoint b) {
-	return CGPointMake(a.x*b.x, a.y*b.y);
-}
+CGSize szScale(    float  s, CGSize  a)    { return CGSizeMake(  s*a.width        ,   s*a.height         ); }
+void   szScaleInpl(float  s, pCGSize a)    { a->width *= s; a->height *= s; }
+CGSize szDiv(     CGSize  a,  float  s)    { return CGSizeMake(    a.width/s      ,     a.height/s       ); }
+void   szDivInpl( pCGSize a,  float  s)    { a->width /= s; a->height /=s; }
+CGSize szCompMul( CGSize  a, CGSize  b)    { return CGSizeMake(    a.width*b.width,     a.height*b.height); }
+CGSize szCompDiv( CGSize  a, CGSize  b)    { return CGSizeMake(    a.width/b.width,     a.height/b.height); }
+CGSize szInv(     CGSize  a)               { return CGSizeMake(1.f/a.width        , 1.f/a.height         ); }
+void   szInvInpl( pCGSize a)               { a->width = 1.f/a->width; a->height = 1.f/a->height; }      
 
-CGPoint compInverseP(CGPoint a) {
-	return CGPointMake(1.f/a.x, 1.f/a.y);
-}
+float  szNorm(CGSize a)                    { return sqrt(a.width*a.width + a.height*a.height); }
+CGSize szNomalize(     CGSize  a)          { float n = sqrt(a.width*a.width + a.height*a.height); return CGSizeMake(a.width/n, a.height/n); }
+void   szNormalizeInpl(pCGSize a)          { float n = sqrt(a->width*a->width + a->height*a->height); a->width /= n; a->height /= n; }
 
-float distP(CGPoint a, CGPoint b) {
-	CGPoint d = diffP(a,b);
-	return sqrt(d.x*d.x+d.y*d.y);
+
+float ptDist(CGPoint a, CGPoint b) {
+    CGPoint d = CGPointMake(-a.x+b.x,-a.y+b.y);
+    return sqrt(d.x*d.x+d.y*d.y);
 }
 
 //   +
@@ -38,9 +50,9 @@ float distP(CGPoint a, CGPoint b) {
 // --+-- +
 // 3 | 4
 
-int quadrantP(CGPoint a) {
-	return (a.y>=0.f)? ((a.x<0.f)? 2 : 1 ) :
-	((a.x<0.f)? 3 : 4 ) ;
+int ptQuadrant(CGPoint a) {
+    return (a.y>=0.f)? ((a.x<0.f)? 2 : 1 ) :
+    ((a.x<0.f)? 3 : 4 ) ;
 }
 
 //     +
@@ -50,22 +62,23 @@ int quadrantP(CGPoint a) {
 //  5 /|\ 8
 //   /6|7\
 
-int octantP(CGPoint a) {
-	float absX = fabs(a.x);
-	float absY = fabs(a.y);
-	switch (quadrantP(a)) {
-		case 1: return (absX>absY)? 1 : 2; break;
-		case 2: return (absX<absY)? 3 : 4; break;
-		case 3: return (absX>absY)? 5 : 6; break;
-		case 4: return (absX<absY)? 7 : 8; break;
-	}
-	return -1;
+int ptOctant(CGPoint a) {
+    float absX = fabs(a.x);
+    float absY = fabs(a.y);
+    switch (ptQuadrant(a)) {
+        case 1: return (absX>absY)? 1 : 2; break;
+        case 2: return (absX<absY)? 3 : 4; break;
+        case 3: return (absX>absY)? 5 : 6; break;
+        case 4: return (absX<absY)? 7 : 8; break;
+    }
+    return -1;
 }
 
-CGSize smulS(float s, CGSize a) {
-	return CGSizeMake(s*a.width,s*a.height);	
+CGRect rectMv(CGRect r, CGPoint p) {
+    return CGRectMake(r.origin.x+p.x, r.origin.y+p.y, r.size.width, r.size.height);
 }
 
-CGRect mvR(CGRect r, CGPoint p) {
-	return CGRectMake(r.origin.x+p.x, r.origin.y+p.y, r.size.width, r.size.height);
+void   rectMvInpl(pCGRect r, CGPoint p) {
+    r->origin.x+=p.x;
+    r->origin.y+=p.y;
 }
